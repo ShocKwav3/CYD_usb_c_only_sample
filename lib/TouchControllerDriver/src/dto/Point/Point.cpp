b7df1f46e2407d55fa038d287b98cb968a172e86
ShocKwav3 <ahmonemon@gmail.com>
2025-02-24 20:26:11 +0200

    updated type to allow bigger number

--- lib/TouchControllerDriver/src/dto/Point/Point.cpp
+++ lib/TouchControllerDriver/src/dto/Point/Point.cpp
@@ -1,3 +1,3 @@
 #include "Point.h"
 
-Point::Point(int x, int y, int z)  : coordinateX(x), coordinateY(y), pressureZ(z) {}
+Point::Point(int x, int y, int z) : coordinateX(x), coordinateY(y), pressureZ(z) {}
